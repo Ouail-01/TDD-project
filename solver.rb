e52646168717d5d71e9be5af3fefbba651137bd6
Roland-Ntwali <rolandntwali@gmail.com>
2023-06-29 10:41:54 +0200

    add reverse

--- solver.rb
+++ solver.rb
@@ -8,4 +8,20 @@ class Solver
       num * factorial(num - 1)
     end
   end
+
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? and (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
